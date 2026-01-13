55b4b429d9a85d516a9144709b674a3147788475
RHW <160179762+rhenryw@users.noreply.github.com>
2026-01-13 16:35:04 +0000

    Updated readme and added cool calendar cause I'm awesome

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,22 @@ import solid from 'vite-plugin-solid'
 
 export default defineConfig({
   plugins: [solid()],
+  resolve: {
+    alias: {
+      'react/jsx-runtime': 'solid-js/jsx-runtime',
+      'react/jsx-dev-runtime': 'solid-js/jsx-dev-runtime',
+      react: 'solid-js',
+    },
+  },
+  optimizeDeps: {
+    rolldownOptions: {
+      resolve: {
+        alias: {
+          'react/jsx-runtime': 'solid-js/jsx-runtime',
+          'react/jsx-dev-runtime': 'solid-js/jsx-dev-runtime',
+          react: 'solid-js',
+        },
+      },
+    },
+  },
 })
